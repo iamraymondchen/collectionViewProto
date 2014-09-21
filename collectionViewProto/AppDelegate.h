bf25df60ae413b01ddc43bb66d2bc889dd8f37ed
Raymond Chen <iamraymondc@gmail.com>
2014-09-21 12:43:31 -0700

    Initial Commit

---
+++ collectionViewProto/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  collectionViewProto
+//
+//  Created by raymond chen on 2014-09-21.
+//  Copyright (c) 2014 raymond chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ collectionViewProto/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  collectionViewProto
+//
+//  Created by raymond chen on 2014-09-21.
+//  Copyright (c) 2014 raymond chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
